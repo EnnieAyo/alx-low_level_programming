aa4aef51610a96c23659f8f4518efac8f8fc66db
Waziri <ayoennie@gmail.com>
2022-10-14 16:27:15 -0700

    adds 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+	return (0);
+}
