18b15b6de78fdd72b0c1032c9f2bc15695e9c932
Waziri <ayoennie@gmail.com>
2022-10-14 17:20:47 -0700

    adds 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	do {
+		putchar(ch);
+		ch--;
+	} while (ch != 'a');
+
+	putchar('\n');
+	return (0);
+}
