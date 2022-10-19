842588a1f89523b5ccb3c088d08dd7b561b3c609
Waziri <ayoennie@gmail.com>
2022-10-19 07:11:59 -0700

    adds 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * times_table - prints a 9 x 9 multiplication
+ * Return: void
+ */
+void times_table(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
