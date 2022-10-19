a55b5da6a4a669c036c9cef062e798b628c2d560
Waziri <ayoennie@gmail.com>
2022-10-19 15:00:20 -0700

    adss 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_times_table - prints times table
  * @n: param
@@ -9,36 +8,30 @@ void print_times_table(int n)
 {
 	int i, j, hundreds, tens, units, product;
 
-	if (n < 16 || n != 0)
+	if (n >= 0 && n < 16)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				product = i * j;
-				hundreds = product / 100;
-				tens = (product % 100) / 10;
-				units = (product % 100) % 10;
-
 				if (j == 0)
-				{
 					_putchar('0');
-				}
 				else if (product >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(hundreds + '0');
-					_putchar(tens + '0');
-					_putchar(units + '0');
+					_putchar((product / 100) + '0');
+					_putchar(((product % 100) / 10) + '0');
+					_putchar(((product % 100) % 10) + '0');
 				}
 				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(tens + '0');
-					_putchar(units + '0');
+					_putchar(((product % 100) / 10) + '0');
+					_putchar(((product % 100) % 10) + '0');
 				}
 				else
 				{
@@ -46,7 +39,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(units + '0');
+					_putchar(((product % 100) % 10) + '0');
 				}
 			}
 			_putchar('\n');
