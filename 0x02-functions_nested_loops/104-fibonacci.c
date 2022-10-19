ebc53858fcbe57f56dee83c095b12d21123ab2fe
Waziri <ayoennie@gmail.com>
2022-10-19 16:25:14 -0700

    adds 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the 1st 50 fib series
+ * Return: void
+ */
+int main(void)
+{
+	int one = 1;
+	int two = 2;
+	long int next;
+	long int hold;
+	long int prev;
+	int i;
+
+	for (i = 0; i < 96; i++)
+	{
+		if (i == 0)
+		{
+			next = one + two;
+			prev = two;
+			printf("1, 2, %ld", next);
+		}
+		else
+		{
+			hold = next;
+			next = prev + next;
+			prev = hold;
+			printf(", %ld", next);
+		}
+	}
+	printf("\n");
+	return (0);
+}
