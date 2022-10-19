c82e9b02936d1eeb09392d00798a1ca54c1e3207
Waziri <ayoennie@gmail.com>
2022-10-19 16:16:46 -0700

    adds 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of the even number in fib series
+ * Return: void
+ */
+int main(void)
+{
+	int one = 1;
+	int two = 2;
+	long int next;
+	long int hold;
+	long int prev;
+	long int i;
+	long long sum = 0;
+
+	for (i = 0; next <= 4000000; i++)
+	{
+		if (i == 0)
+		{
+			next = one + two;
+			prev = two;
+			sum = two;
+		}
+		else
+		{
+			hold = next;
+			next = prev + next;
+			prev = hold;
+			if (next % 2 == 0)
+			sum = sum + next;
+		}
+	}
+	printf(sum);
+	printf("\n");
+	return (0);
+}
