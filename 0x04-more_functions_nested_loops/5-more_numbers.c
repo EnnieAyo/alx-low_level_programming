8bc25af435f9b08c0b40499d1896ef6cdaef408e
Waziri <ayoennie@gmail.com>
2022-10-20 14:53:08 -0700

    updates

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,15 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int tens;
-	int units;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			tens = j / 10;
-			units = j % 10;
 			if (j > 9)
 			{
-				_putchar(tens + 48);
-				_putchar(units + 48);
+				_putchar((j / 10)  + 48);
+				_putchar((j % 10) + 48);
 			}
 			_putchar('\n');
 		}
