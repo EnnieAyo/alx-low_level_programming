3042de13657105a8bb687cc23b0092e36227cf73
Waziri <ayoennie@gmail.com>
2022-10-20 04:58:25 -0700

    adds  3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: string
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
