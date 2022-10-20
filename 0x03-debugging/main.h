0cd4f0a00d879058c32b5a904aca069fc4331a4b
Waziri <ayoennie@gmail.com>
2022-10-20 15:50:03 -0700

    updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)i;
+	void positive_or_negative(int i)i;
 
-int largest_number(int a, int b, int c);
+	int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
