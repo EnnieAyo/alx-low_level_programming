06c75b9af1399699f32a806cb5657c498888d6c4
Waziri <ayoennie@gmail.com>
2022-10-20 12:27:57 -0700

    updates print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,23 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int i = '0';
+	int i;
 
-	while (i <= '9')
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == '2')
-		{
-			continue;
-		}
-		else if (i == '4')
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(i);
-		}
-		i++;
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
