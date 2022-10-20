6b8e4f8eb62a73f6270f9b6d809ff61a87eff4f6
Waziri <ayoennie@gmail.com>
2022-10-20 05:23:26 -0700

    adds 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ * Return: string
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		else
+		{
+			continue;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
