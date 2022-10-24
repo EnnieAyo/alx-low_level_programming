ced67e74ae47210704b65c5b7ef397f92fc4d055
Waziri <ayoennie@gmail.com>
2022-10-24 14:47:23 -0700

    adds 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -  a function to fint the length of a string
+ * @s: param
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int cnt = strlen(*s);
+
+	return (cnt);
+}
