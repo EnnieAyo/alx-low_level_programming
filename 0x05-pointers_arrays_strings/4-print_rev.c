d4a59e1e17e6d62248cf307ced77bb5fe6540423
Waziri <ayoennie@gmail.com>
2022-10-24 15:15:44 -0700

    adds 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - a function to print the reverse of a string
+ * @s: param
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i;
+
+	for (i = strlen(s); strlen(s) >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
