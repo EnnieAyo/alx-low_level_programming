67ead2d87689756ad99c6c5a1a85c6db78f45fd7
Waziri <ayoennie@gmail.com>
2022-10-27 07:14:47 -0700

    adds 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - a function to copy a string
+ * @dest: param
+ * @src: param
+ * @n: param
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char hold[4000];
+int i;
+
+        for (i = 0; i <= (int) strlen(s); i++)
+        {
+                hold[i] = s[i];
+        }
+	return (hold);
+}
