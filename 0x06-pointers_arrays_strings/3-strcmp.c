7a47c91135c4b70fa7820cd6c932adf1c3f6c195
Waziri <ayoennie@gmail.com>
2022-10-27 08:50:37 -0700

    adds 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,28 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-int first = 0;
-int second = 0;
+	int i;
+	int first = 0;
+	int second = 0;
 
-for (i = 0; i <= strlen(s1) && s1[i] != '\0'; i++)
-{
-first = first + (int)s1[i];
-}
-for (i = 0; i <= strlen(s2) && s2[i] != '\0'; i++)
-{
-second = second + (int)s2[i];
-}
-/*printf("first = %d, second = %d\n", first, second);*/
-if (first - second < 0)
-{
-return (-15);
-}
-else if (first - second > 0)
-{
-return (15);
-}
-else
-{
-return (0);
-}
+	for (i = 0; i <= strlen(s1) && s1[i] != '\0'; i++)
+	{
+		first = first + (int) s1[i];
+	}
+	for (i = 0; i <= strlen(s2) && s2[i] != '\0'; i++)
+	{
+		second = second + (int) s2[i];
+	}
+	if (first - second < 0)
+	{
+		return (-15);
+	}
+	else if (first - second > 0)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
 }
