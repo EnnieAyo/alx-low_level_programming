42b4aea5bfb731d2b3e3cadc809a7b82a80910cb
Waziri <ayoennie@gmail.com>
2022-10-27 06:48:19 -0700

    adds 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * char *_strcat - a function to print the reverse of a string
+ * @dest: param (destination)
+ * @src: param (source)
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
