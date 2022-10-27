9abb7451ef0f81873198195a820ef2a9b3db6ef3
Waziri <ayoennie@gmail.com>
2022-10-27 03:51:02 -0700

    adds 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - a function to print the reverse of a string
+ * @s: param
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char hold[] = s;
+int i;
+
+	for (i = sizeof(s) - 2; i >= 0; i--)
+	{
+		*(s + i) = hold[sizeof(hold - 2 - i)]
+	}
+}
