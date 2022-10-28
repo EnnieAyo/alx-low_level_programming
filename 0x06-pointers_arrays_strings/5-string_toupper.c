a313e536e775d192b9368e3793a676668f6c91fe
Waziri <ayoennie@gmail.com>
2022-10-28 03:10:44 -0700

    adds 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * **string_toupper - a function to uppercase of a string
+ * @s: param
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+int i;
+
+for (i = 0; i < (int) strlen(s) && s[i] != '\0'; i++)
+{
+s[i] = toupper(s[i]);
+}
+return (s);
+}
