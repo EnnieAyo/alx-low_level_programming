10d20fe775a3c596282bacb63168205e6e8d003b
Waziri <ayoennie@gmail.com>
2022-11-03 12:05:32 -0700

    adds 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - a function to search a string
+ * @s: param (pointer)
+ * @c: param (char)
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+char *n;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+n = s + i;
+break;
+}
+else
+n = '\0';
+}
+return (n);
+}
