b859d1bb6a44c40ed800beb373c89562044b9c80
Waziri <ayoennie@gmail.com>
2022-11-03 14:47:23 -0700

    adds advance task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set-string - set the value of a string
+ * @s: param (pointer to a pointer)
+ * @to: param pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
